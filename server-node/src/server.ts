1ab8d876beae174d875d997b21b90e3b044d9801
LuisFelipeSalvarani <felipe_salvarani@hotmail.com>
2024-09-20 20:42:57 -0300

    chore: configuração incial do fastify

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,1 +1,15 @@
-console.log('Hello world')
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'Hello NLW Unite'
+})
+
+app.get('/teste', () => {
+  return 'Hello Teste'
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HHTP serving running')
+})
