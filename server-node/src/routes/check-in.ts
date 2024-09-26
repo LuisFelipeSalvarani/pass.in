2f05b7c584ebbb38a397b68f50c202d6b42dee3d
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-26 13:44:26 -0300

    feat: criação da rota de check-in

---
+++ server-node/src/routes/check-in.ts
@@ -0,0 +1,41 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function checkIn(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/attendees/:attendeeId/check-in',
+    {
+      schema: {
+        params: z.object({
+          attendeeId: z.coerce.number().int(),
+        }),
+        response: {
+          201: z.null,
+        },
+      },
+    },
+    async (request, reply) => {
+      const { attendeeId } = request.params
+
+      const attendeeCheckIn = await prisma.chekIn.findUnique({
+        where: {
+          attendeeId,
+        },
+      })
+
+      if (attendeeCheckIn !== null) {
+        throw new Error('Attendee already checked in!')
+      }
+
+      await prisma.chekIn.create({
+        data: {
+          attendeeId,
+        },
+      })
+
+      return reply.status(201).send()
+    }
+  )
+}
