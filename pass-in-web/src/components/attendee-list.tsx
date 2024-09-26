a97e5c0ee32034a49352c2375e46b3446430ee99
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-26 16:50:47 -0300

    feat: estilização do título da página e do campo de busca

--- pass-in-web/src/components/attendee-list.tsx
+++ pass-in-web/src/components/attendee-list.tsx
@@ -1,3 +1,17 @@
+import { Search } from 'lucide-react'
+
 export function AttendeeList() {
-  return <h1>Participnates</h1>
+  return (
+    <div className="flex items-center gap-3">
+      <h1 className="font-bold text-2xl">Participantes</h1>
+      <div className="flex w-72 items-center gap-3 rounded-lg border border-white/10 px-3 py-1.5 text-sm">
+        <Search className="size-4 text-emerald-300" />
+        <input
+          type="text"
+          className="flex-1 bg-transparent outline-none"
+          placeholder="Buscar participante..."
+        />
+      </div>
+    </div>
+  )
 }
