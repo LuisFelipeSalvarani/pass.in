98d5500c6449025e138661cd52d7ebf79f3fc78f
Luis Felipe Salvarani <55268395+LuisFelipeSalvarani@users.noreply.github.com>
2024-09-26 15:15:09 -0300

    chore: configurações de scripts e primeiro build

---
+++ server-node/dist/server.mjs
@@ -0,0 +1,69 @@
+import {
+  registerForEvent
+} from "./chunk-NHJJMMEC.mjs";
+import {
+  errorHandler
+} from "./chunk-SJUXIQ4B.mjs";
+import {
+  checkIn
+} from "./chunk-KNCEDJBN.mjs";
+import {
+  createEvent
+} from "./chunk-I66ULZK6.mjs";
+import "./chunk-KDMJHR3Z.mjs";
+import {
+  getAttendeeBadge
+} from "./chunk-6BUMJQIW.mjs";
+import {
+  getEventAttendees
+} from "./chunk-RLNX4GEL.mjs";
+import {
+  getEvent
+} from "./chunk-5EPCSY3M.mjs";
+import "./chunk-JRO4E4TH.mjs";
+import "./chunk-JV6GRE7Y.mjs";
+
+// src/server.ts
+import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler
+} from "fastify-type-provider-zod";
+var app = fastify().withTypeProvider();
+app.register(fastifyCors, {
+  origin: "*"
+});
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description: "Especifica\xE7\xF5es da API para o back-end da aplica\xE7\xE3o pass.in construida durante o NLW Unite da Rocketseat.",
+      version: "1.0.0"
+    }
+  },
+  transform: jsonSchemaTransform
+});
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs"
+});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+app.register(createEvent);
+app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeeBadge);
+app.register(checkIn);
+app.register(getEventAttendees);
+app.setErrorHandler(errorHandler);
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP serving running");
+});
+export {
+  app
+};
